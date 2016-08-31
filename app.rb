358355944022fc376a3d3f6c301c60e39c3b3aad
James Stonehill <jamesstonehill@gmail.com>
2016-08-31 15:55:49 +0100

    now using session vars instead of instance vars

--- app.rb
+++ app.rb
@@ -1,14 +1,26 @@
 require 'sinatra/base'
 
+
+
 class Battle < Sinatra::Base
+  enable :sessions
+
   get '/' do
     erb(:index)
   end
 
-  post '/names' do
-    @name1 = params[:user_name_one]
-    @name2 = params[:user_name_two]
+  get '/play' do
+    @user_name_one = session['user_name_one']
+    @user_name_two = session['user_name_two']
     erb(:play)
+  end
+
+  post '/names' do
+    session['user_name_one'] = params[:user_name_one]
+    session['user_name_two'] = params[:user_name_two]
+    # @name1 = params[:user_name_one]
+    # @name2 = params[:user_name_two]
+    redirect '/play'
   end
 
   # start the server if ruby file executed directly
