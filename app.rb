e0850565effec36847d7b16676feab56a9ded4fe
Tam Borine <tamborine@Tams-MacBook-Air.local>
2016-09-01 12:43:28 +0100

    added player objects in controller

--- app.rb
+++ app.rb
@@ -1,28 +1,28 @@
 require 'sinatra/base'
+require_relative './lib/player'
 
 class Battle < Sinatra::Base
-  enable :sessions
 
   get '/' do
     erb(:index)
   end
 
+  post '/names' do
+    $p1 = Player.new(params[:user_name_one])
+    $p2 = Player.new(params[:user_name_two])
+    redirect '/play'
+  end
+
   get '/play' do
-    @user_name_one = session['user_name_one']
-    @user_name_two = session['user_name_two']
+    @user_name_one = $p1.name
+    @user_name_two = $p2.name
     erb(:play)
   end
 
   get '/attack' do
     erb(:attack)
   end
-
-  post '/names' do
-    session['user_name_one'] = params[:user_name_one]
-    session['user_name_two'] = params[:user_name_two]
-    redirect '/play'
-  end
-
+  
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
